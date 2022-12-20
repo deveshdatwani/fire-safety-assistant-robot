b590b83bf5cc13cd41827794c049bf0a39f8dd85
devesh datwani <datwanidevesh@gmail.com>
2022-12-20 00:40:57 -0500

    forked from og

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/deveshdatwani/capstone/src
+# Build directory: /home/deveshdatwani/capstone/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+<<<<<<< HEAD
+subdirs("autonomous_tb")
+subdirs("change_map")
+=======
+>>>>>>> a866c9ff26d8b0371b9cf95afc0e5e6261580df8
+subdirs("statemachine")
